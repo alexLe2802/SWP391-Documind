9566611355cfa867c08a65f27d5bddb0fa5ae357
Nguyen-dev123 <phamducnguyen3456@gmail.com>
2026-08-22 08:36:24 +0700

    reconstruct(mf-05): restore admin document moderation listing

--- be/src/admin/admin.module.ts
+++ be/src/admin/admin.module.ts
@@ -1,8 +1,9 @@
 ﻿import { Module } from '@nestjs/common';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { AdminUsersModule } from './users/admin-users.module';
+import { AdminDocumentsModule } from './documents/admin-documents.module';
 
 @Module({
-  imports: [DashboardModule, AdminUsersModule],
+  imports: [DashboardModule, AdminUsersModule, AdminDocumentsModule],
 })
 export class AdminModule {}
